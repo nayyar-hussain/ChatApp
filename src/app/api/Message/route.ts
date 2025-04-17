089752625eca6c759382354808a1b9244cee14c8
Nayyar Hussain <se.nayyarhussain@gmail.com>
2025-04-17 12:05:22 +0500

    flie delted

--- src/app/api/Message/route.ts
+++
@@ -1,9 +0,0 @@
-// import { NextRequest, NextResponse } from "next/server";
-
-// export async function POST(req: NextRequest) {
-//     try {
-        
-//     } catch (error) {
-//         return NextResponse.json({status : 500 , error})
-//     }
-// }
